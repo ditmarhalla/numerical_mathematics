d63276c9f5ced5a948c8fa850f2497483ff30d17
Ditmar <ditmarhalla@protonmail.ch>
2021-04-19 00:46:25 +0200

    'fix'

--- Report_1_Task_6.m
+++ Report_1_Task_6.m
@@ -3,7 +3,7 @@ clc
 
 f = @(x) log10(x)+cos(x)-1;
 df =@(x) 1/x*log(10)-sin(x);
-x = 7;
+x = 6;
 %dif =@(x) diff(f2);
 y= x-f(x)/df(x);
 root = 1;
@@ -12,7 +12,7 @@ epsilon = 1e-7;
 while root > epsilon
     x1=x-f(x)/df(x);
     x = x1;
-    root = abs(x-x1);
+    root = abs(f(x1));
     s = x1;
 end
 
